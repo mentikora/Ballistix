a19ca82bb68fbd45fcc962fbae894c7200ea9d6f
Mentikora <notender_boy@ukr.net>
2016-07-07 18:22:54 +0300

    menu

--- js/application.js
+++ js/application.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+  // initialize fullpage js
   $('#fullpage').fullpage({
-  	normalScrollElements: '.test'
+    normalScrollElements: '#menu'
+  });
+
+  // show menu
+  $('#menu_btn').on('click', function(){
+    $('#menu').addClass('fadeInLeft').removeClass('fadeOutLeft').css('display', 'block');
+  });
+
+  // close menu
+  $('.close_menu').on('click', function(){
+    $('#menu').addClass('fadeOutLeft').removeClass('fadeInLeft');
   });
 });
