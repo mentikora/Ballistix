7fa7abfa3c0501b71cb7db3035c5a15097013947
Mentikora <notender_boy@ukr.net>
2016-07-08 16:33:02 +0300

    border_btn effect, not finished

--- js/application.js
+++ js/application.js
@@ -28,4 +28,18 @@ $(document).ready(function() {
   $('#popUp .close_menu').on('click', function(){
     $('#popUp').addClass('slideOutLeft').removeClass('slideInLeft');
   });
+
+  // border-btn animation
+  $('.border_btn').on('mouseenter', function(e) {
+    var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+    $(this).find('span').css({top:relY, left:relX})
+  })
+  .on('mouseout', function(e) {
+    var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+    $(this).find('span').css({top:relY, left:relX})
+  });
 });
