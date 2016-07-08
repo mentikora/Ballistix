e4eccce048dd31140011d2c3c80c423e2387d7d3
Mentikora <notender_boy@ukr.net>
2016-07-08 17:54:57 +0300

    smth

--- js/application.js
+++ js/application.js
@@ -29,20 +29,6 @@ $(document).ready(function() {
     $('#popUp').addClass('slideOutLeft').removeClass('slideInLeft');
   });
 
-  // border-btn animation
-  // $('.border_btn').on('mouseenter', function(e) {
-  //   var parentOffset = $(this).offset(),
-  //       relX = e.pageX - parentOffset.left,
-  //       relY = e.pageY - parentOffset.top;
-  //   $(this).find('span').css({top:relY, left:relX})
-  // })
-  // .on('mouseout', function(e) {
-  //   var parentOffset = $(this).offset(),
-  //       relX = e.pageX - parentOffset.left,
-  //       relY = e.pageY - parentOffset.top;
-  //   $(this).find('span').css({top:relY, left:relX})
-  // });
-
   // share button header
   
 });
